22812eb4bc2624a3d38d438021cdee84ff13fa42
williamgdo <williamgdo.app@gmail.com>
2023-04-01 08:20:32 -0300

    feat: changes backend every call

--- load-balancer.py
+++ load-balancer.py
@@ -1,28 +1,55 @@
 # basic server example
 
 import requests
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 # loadbalancer = Flask(__name__)
 
-BACKENDS = ['http://power1:5000', 'http://power2:5000']
-# 5000 is the default port for flask applications 
+BACKENDS = ["http://service-a:5000", "http://service-b:5000"]
+# 5000 is the default port for flask applications
+CURRENT_BACKEND = 0
 
-  
-@app.route('/power', methods=['POST'])
-def toDistribute():
-  x = 2
-  y = 4
-  dictToSend = { x, y }
-  
-  try:
-    res = requests.post('http://service/calculate', json=dictToSend)
 
-    print ('response from server:',res.text)
-    dictFromServer = res.json()
-  except:
-    print("An exception occurred") 
+@app.route("/power", methods=["POST"])
+def postPower():
+    global CURRENT_BACKEND
+    inputJson = request.get_json(force=True)
+    # force=True, above, is necessary if another developer
+    # forgot to set the MIME type to 'application/json'
 
-if __name__ == '__main__':
-  app.run(host='0.0.0.0')
+    x = inputJson["x"]
+    y = inputJson["y"]
+
+    dictToSend = {"x": x, "y": y}
+
+    try:
+        res = requests.post(BACKENDS[CURRENT_BACKEND] + "/calculate", json=dictToSend)
+
+        CURRENT_BACKEND = (CURRENT_BACKEND + 1) % 2
+
+        dictFromServer = res.json()
+        return dictFromServer
+    except Exception as e:
+        print("An exception occurred: " + str(e))
+        return jsonify(message="An exception occurred: " + str(e))
+
+
+@app.route("/power", methods=["GET"])
+def getPower():
+    global CURRENT_BACKEND
+
+    try:
+        res = requests.get(BACKENDS[CURRENT_BACKEND] + "/values")
+
+        CURRENT_BACKEND = (CURRENT_BACKEND + 1) % 2
+
+        dictFromServer = res.json()
+        return dictFromServer
+    except Exception as e:
+        print("An exception occurred: " + str(e))
+        return jsonify(message="An exception occurred: " + str(e))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
